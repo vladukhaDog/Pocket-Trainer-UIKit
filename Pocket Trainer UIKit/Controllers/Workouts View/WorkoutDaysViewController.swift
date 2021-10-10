7e71395099b248096700c715baa56bf18224045f
vladukhaDog <vladic.p@gmail.com>
2021-10-10 15:29:35 +0300

    implemented segmented control

--- Pocket Trainer UIKit/Controllers/Workouts View/WorkoutDaysViewController.swift
+++ Pocket Trainer UIKit/Controllers/Workouts View/WorkoutDaysViewController.swift
@@ -11,6 +11,8 @@ class WorkoutDaysViewController: UIViewController {
 
 	//MARK: - data vars
 	var workout: Workout!
+	private var workoutDays = [WorkoutDay]()
+	private var currentWorkoutDay: WorkoutDay?
 	
 	//MARK: - UI elements
 
@@ -24,18 +26,48 @@ class WorkoutDaysViewController: UIViewController {
 		return stackView
 	}()
 	
+	private lazy var segmentControl: UISegmentedControl = {
+		let seg = UISegmentedControl()
+		seg.frame.size = CGSize(width: 0, height: 30)
+		seg.translatesAutoresizingMaskIntoConstraints = false
+		
+		return seg
+	}()
+	
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-	
+		
 		setupViews()
         setConstraints()
+		startFetch()
     }
 	
 	//MARK: - functions
 	private func setupViews(){
+		view.backgroundColor = UIColor(named: "Background")
+		navigationItem.largeTitleDisplayMode = .never
 		view.addSubview(stackView)
+		stackView.addArrangedSubview(segmentControl)
+		segmentControl.addTarget(self, action: #selector(self.segmentedValueChanged(_:)), for: .valueChanged)
+		segmentControl.selectedSegmentIndex = 0
+	}
+	
+	@objc func segmentedValueChanged(_ sender:UISegmentedControl!)
+	   {
+		   print("Selected Segment Index is : \(sender.selectedSegmentIndex)")
+	   }
+	
+	private func startFetch(){
+		getWorkoutDays(complete: {
+			self.workoutDays = $0.filter({workoutDay in
+				self.workout.WorkoutDays.contains(where: {$0.WorkoutDayID == workoutDay.WorkoutDayID})
+			})
+			for index in 0..<self.workoutDays.count {
+				self.segmentControl.insertSegment(withTitle: self.workoutDays[index].Name ?? "", at: index, animated: true)
+			}
+			self.segmentControl.selectedSegmentIndex = 0
+		})
 	}
 	
 	
@@ -46,10 +78,20 @@ extension WorkoutDaysViewController{
 	private func setConstraints() {
 		//sstackview constraints
 		NSLayoutConstraint.activate([
-			stackView.topAnchor.constraint(equalTo: view.topAnchor),
+			stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
 			stackView.rightAnchor.constraint(equalTo: view.rightAnchor),
 			stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
 			stackView.leftAnchor.constraint(equalTo: view.leftAnchor)
 		])
+		
+		NSLayoutConstraint.activate([
+			segmentControl.topAnchor.constraint(equalTo: stackView.topAnchor),
+			segmentControl.widthAnchor.constraint(equalTo: stackView.widthAnchor, constant: -60),
+			segmentControl.heightAnchor.constraint(equalTo: stackView.widthAnchor, constant: -370)
+			
+		])
 	}
 }
+
+
+
