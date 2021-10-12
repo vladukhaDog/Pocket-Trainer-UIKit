169d1418551a1de45fd5ac8f6d81b2c08f89dcea
vladukhaDog <vladic.p@gmail.com>
2021-10-12 03:05:56 +0300

    made functional search bar when adding an exercise in calendar

--- Pocket Trainer UIKit/Controllers/Calendar/CalendarViewController.swift
+++ Pocket Trainer UIKit/Controllers/Calendar/CalendarViewController.swift
@@ -21,7 +21,18 @@ class CalendarViewController: UIViewController {
 		
 		return picker
 	}()
+	
+	private var addButton: UIButton = {
+		let button = UIButton()
+		button.setTitle("Добавить Упражнение", for: .normal)
+		button.setTitleColor(UIColor.systemBlue, for: .normal)
+		button.setTitleColor(UIColor.systemBlue.withAlphaComponent(0.4), for: .highlighted)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		return button
+	}()
 
+	
+	//MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,14 +40,18 @@ class CalendarViewController: UIViewController {
 		setConstraints()
     }
     
-	
-	@objc func datePickerValueChanged(_ sender: UIDatePicker){
+	//MARK: - functions
+	@objc private func datePickerValueChanged(_ sender: UIDatePicker){
 		dateChange()
 	
 	}
 	private func dateChange(){
 		exercises = db.getExercisesByDate(datePicker.date)
 		print("Changed date")
+	}
+	
+	@objc private func buttonPressed(_ sender: UIButton){
+		self.present(ChooseExerciseViewController(), animated: true)
 	}
 	
 	
@@ -50,14 +65,23 @@ extension CalendarViewController{
 	
 	private func setupViews(){
 		view.addSubview(datePicker)
+		view.addSubview(addButton)
+		view.backgroundColor = UIColor(named: "Background")
+		title = ""
 		datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
+		addButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
 		exercises = db.getExercisesByDate(Date())
 	}
 	
+	//MARK: - Constraints
 	private func setConstraints(){
 		NSLayoutConstraint.activate([
-			datePicker.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-			datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+			datePicker.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+			datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+		])
+		NSLayoutConstraint.activate([
+			addButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+			addButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
 		])
 	}
 }
---
+++ Pocket Trainer UIKit/Controllers/Calendar/ChooseExerciseViewController.swift
@@ -0,0 +1,144 @@
+//
+//  ChooseExerciseViewController.swift
+//  Pocket Trainer UIKit
+//
+//  Created by vladukha on 11.10.2021.
+//
+
+import UIKit
+
+class ChooseExerciseViewController: UIViewController {
+	private var exercises = [Exercise]()
+	private var exercisesFiltered = [Exercise]()
+	
+	//MARK: - UI elements
+	private let collectionView: UICollectionView = {
+		let layout = UICollectionViewFlowLayout()
+		layout.minimumLineSpacing = 15.0
+		let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+		//collectionView.backgroundColor = UIColor(named: "Background")
+		collectionView.bounces = true
+		collectionView.register(ExerciseCollectionViewCell.self, forCellWithReuseIdentifier: ExerciseCollectionViewCell.identifier)
+		collectionView.translatesAutoresizingMaskIntoConstraints = false
+		return collectionView
+	}()
+	
+	private let searchBar: UISearchBar = {
+		let bar = UISearchBar()
+		bar.translatesAutoresizingMaskIntoConstraints = false
+		bar.placeholder = "Поиск упражнения..."
+		bar.enablesReturnKeyAutomatically = false
+		return bar
+	}()
+	
+	
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		setupViews()
+		setConstraints()
+		setDelegates()
+		
+		startFetch()
+    }
+    
+
+
+}
+
+
+//MARK: - initial setups
+extension ChooseExerciseViewController{
+	private func setupViews(){
+		view.backgroundColor = UIColor(named: "Background")
+		view.addSubview(searchBar)
+		view.addSubview(collectionView)
+	}
+	
+	private func setDelegates(){
+		
+		collectionView.delegate = self
+		collectionView.dataSource = self
+		collectionView.delaysContentTouches = false
+		searchBar.delegate = self
+		
+	}
+	
+	//fetching Exercises from API
+	private func startFetch(){
+		getExercises(complete: {(cum) in
+			self.exercises = cum
+			self.exercisesFiltered = cum
+			self.collectionView.reloadData()
+			})
+			
+		
+	}
+	
+	private func setConstraints(){
+		NSLayoutConstraint.activate([
+			searchBar.topAnchor.constraint(equalTo: view.topAnchor),
+			searchBar.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30),
+			searchBar.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+		])
+		
+		NSLayoutConstraint.activate([
+			collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+			collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+			collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+			collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+		])
+		
+	}
+}
+
+
+//MARK: - CollectionView Delegate & DataSource
+extension ChooseExerciseViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return exercisesFiltered.count
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ExerciseCollectionViewCell.identifier, for: indexPath) as? ExerciseCollectionViewCell{
+			cell.exerciseData = exercisesFiltered[indexPath.row]
+		return cell
+		}
+		return UICollectionViewCell()
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		print("pressed")
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		return CGSize(
+			
+					width: collectionView.frame.width-20,
+					height: self.view.bounds.width/5
+				)
+		
+		
+	}
+	
+	
+}
+
+
+//MARK: - SearchBar delegate
+extension ChooseExerciseViewController: UISearchBarDelegate{
+	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+			exercisesFiltered = searchText.isEmpty ? exercises : exercises.filter { (item: Exercise) -> Bool in
+				// If dataItem matches the searchText, return true to include it
+				return item.Name.lowercased().contains(searchText.lowercased())
+			}
+			
+			collectionView.reloadData()
+		}
+	
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		searchBar.resignFirstResponder()
+	}
+}
